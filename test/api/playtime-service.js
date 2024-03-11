eaaf9d9f49d72be4e35df1b9c5646964e925a26c
oksana.misiura.cld@gmail.com <oksana.misiura.cld@gmail.com>
2024-03-11 22:45:30 +0000

    add playlist api tdd

--- test/api/playtime-service.js
+++ test/api/playtime-service.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+
 import { serviceUrl } from "../fixtures.js";
 
 export const playtimeService = {
@@ -46,31 +47,6 @@ export const playtimeService = {
 
   async getPlaylist(id) {
     const res = await axios.get(`${this.playtimeUrl}/api/playlists/${id}`);
-    return res.data;
-  },
-
-  async getAllTracks() {
-    const res = await axios.get(`${this.playtimeUrl}/api/tracks`);
-    return res.data;
-  },
-
-  async createTrack(id, track) {
-    const res = await axios.post(`${this.playtimeUrl}/api/playlists/${id}/tracks`, track);
-    return res.data;
-  },
-
-  async deleteAllTracks() {
-    const res = await axios.delete(`${this.playtimeUrl}/api/tracks`);
-    return res.data;
-  },
-
-  async getTrack(id) {
-    const res = await axios.get(`${this.playtimeUrl}/api/tracks/${id}`);
-    return res.data;
-  },
-
-  async deleteTrack(id) {
-    const res = await axios.delete(`${this.playtimeUrl}/api/tracks/${id}`);
     return res.data;
   },
 };

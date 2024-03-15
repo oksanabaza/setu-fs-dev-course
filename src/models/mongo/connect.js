7d524b74be13c6fa13ea50facf6b7575ada4bde3
oksana.misiura.cld@gmail.com <oksana.misiura.cld@gmail.com>
2024-03-15 12:43:06 +0000

    seed data

--- src/models/mongo/connect.js
+++ src/models/mongo/connect.js
@@ -1,5 +1,15 @@
 import * as dotenv from "dotenv";
 import Mongoose from "mongoose";
+import * as mongooseSeeder from "mais-mongoose-seeder";
+import { seedData } from "./seed-data.js";
+
+const seedLib = mongooseSeeder.default;
+
+async function seed() {
+  const seeder = seedLib(Mongoose);
+  const dbData = await seeder.seed(seedData, { dropDatabase: false, dropCollections: true });
+  console.log(dbData);
+}
 
 export function connectMongo() {
   dotenv.config();
@@ -16,7 +26,8 @@ export function connectMongo() {
     console.log("database disconnected");
   });
 
-  db.once("open", function () {
+  db.once("open", function() {
     console.log(`database connected to ${this.name} on ${this.host}`);
+    seed();
   });
 }
